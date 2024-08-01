2ffd30fee06a8e7e7f6b3d5af4356c8403e8c9f9
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-01 17:23:33 +0500

    Form desing complete

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -14,29 +14,42 @@ export async function action({ request }) {
 export default function InputForm() {
   return (
     <div className="form-container">
-      <Form method="post">
-        <input type="color" name="color" id="color" defaultValue="#42bff5" />
-        <select name="mode" id="mode" defaultValue="monochrome">
-          <option value="monochrome">Monochrome</option>
-          <option value="monochrome-dark">Monochrome Dark</option>
-          <option value="monochrome-light">Monochrome Light</option>
-          <option value="analogic">Analogic</option>
-          <option value="complement">Complement</option>
-          <option value="analogic-complement">Analogic Complement</option>
-          <option value="triad">Triad</option>
-          <option value="quad">Quad</option>
-        </select>
-        <label htmlFor="count">Count</label>
+      <Form method="post" className="form">
         <input
-          type="number"
-          name="count"
-          id="count"
-          max={50}
-          min={1}
-          defaultValue="5"
-          required
+          type="color"
+          name="color"
+          id="color"
+          defaultValue="#42bff5"
+          aria-label="color"
+
         />
-        <button>Get color scheme</button>
+        <div className="inputContainer">
+          <label htmlFor="mode">Mode</label>
+          <select name="mode" id="mode" defaultValue="monochrome" className="border">
+            <option value="monochrome">Monochrome</option>
+            <option value="monochrome-dark">Monochrome Dark</option>
+            <option value="monochrome-light">Monochrome Light</option>
+            <option value="analogic">Analogic</option>
+            <option value="complement">Complement</option>
+            <option value="analogic-complement">Analogic Complement</option>
+            <option value="triad">Triad</option>
+            <option value="quad">Quad</option>
+          </select>
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="count">Count</label>
+          <input
+            type="number"
+            name="count"
+            id="count"
+            max={50}
+            min={1}
+            defaultValue="5"
+            className="border"
+            required
+          />
+        </div>
+        <button className="btn border">Get scheme</button>
       </Form>
     </div>
   );
