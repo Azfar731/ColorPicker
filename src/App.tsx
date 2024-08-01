5a5b6661f13db34f6c9b58232b4a8dc15ef48188
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-01 17:49:04 +0500

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import HomePage, { loader as HomePageLoader } from "./Homepage";
 import InputForm, { action as InputFormAction } from "./InputForm";
-import Pallet from "./Pallet";
+import Navbar from "./Navbar";
 import {
   Route,
   RouterProvider,
@@ -18,7 +18,12 @@ function App() {
     )
   );
 
-  return <RouterProvider router={browserRouter} />;
+  return (
+    <>
+      <Navbar />
+      <RouterProvider router={browserRouter} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Color Scheme Generator</h1>
+    </nav>
+  )
+}
