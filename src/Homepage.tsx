af44fa58eafbd30e1d4988533921c2726ec439e3
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-05 18:43:51 +0500

    Deferred Data implemented but not running properly

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,43 +1,58 @@
-import InputForm from "./InputForm";
 import Pallet from "./Pallet";
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, defer, Await ,useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 import "./HomePage.css";
 
-export async function loader({ request }) {
-  console.log("this change is in my dev branch")
-  const params = new URL(request.url).searchParams;
-  const color = params.get("color") || "42bff5";
-  const mode = params.get("mode") || "monochrome";
-  const count = params.get("count") || "5";
 
-  const baseUrl = "https://www.thecolorapi.com/scheme";
-  const searchQuery = `?hex=${color}&mode=${mode}&count=${count}&format=json`;
 
-  console.log("URL: " + baseUrl + searchQuery);
-  
-  const response = await fetch(baseUrl + searchQuery);
+async function fetchDataFromColorAPI(targetUrl: string){
+  const response = await fetch(targetUrl);
   if (response.status !== 200) {
     throw new Error("Error occurred with API");
   }
   const data = await response.json();
-  data?.colors
-    ? console.log(data.colors)
-    : console.log("Resposne is 200 but colors array not available");
-  return data.colors;
+ 
+  console.log('data fetched')
+  return data.colors
+}
+export function loader({ request }) {
+  const params = new URL(request.url).searchParams;
+  const color = params.get("color") || "42bff5";
+  const mode = params.get("mode") || "monochrome";
+  const count = params.get("count") || "5";
+  
+  const baseUrl = "https://www.thecolorapi.com/scheme";
+  const searchQuery = `?hex=${color}&mode=${mode}&count=${count}&format=json`;
+  
+  console.log("Loader function running")
+ 
+  return defer({colorsArray: fetchDataFromColorAPI(baseUrl + searchQuery)});
 }
 
-export default function HomePage() {
-  const colorArray = useLoaderData();
 
-  const colorInfo = colorArray.map((color) => ({hex: color.hex.value, name: color.name.value}));
-  // console.log(colorHexValues)
+export default function HomePage() {
+  const loaderData = useLoaderData();
+  console.log(loaderData.colorsArray)
+  console.log("loaderData variable accessed")
+  
+
+  function renderPallet(colorsArray){
+    const colorInfo = colorsArray.map((color) => ({hex: color.hex.value, name: color.name.value}));
+    console.log("In render function")
+    console.log(colorInfo)
+    return(
+      <Pallet colorsInfo={colorInfo} />
+    )
+  }
 
   return (
     <main>
       <Outlet />
-      <Pallet colorsInfo={colorInfo} />
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Await resolve={loaderData.colorsArray}>
+          {renderPallet}
+        </Await>
+      </Suspense>
     </main>
   );
 }
-
-// export { loader, action };
