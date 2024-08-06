26ee2a586d61027c280524b5f3a747f88718b2e8
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-06 15:14:12 +0500

    Implented clipboard functionality

--- src/ColorDisplayer.tsx
+++ src/ColorDisplayer.tsx
@@ -19,12 +19,27 @@ export default function ColorDisplayer({ colorInfo, width }: Params) {
     fontSize: width * 0.2 > 2 ? `2rem` : `1.5rem`,
   };
 
+  async function handelClick(){
+    if(navigator.clipboard){
+     navigator.clipboard.writeText(colorInfo.hex).then(()=> {
+      const prompt = document.getElementById(`value-copied-prompt-${colorInfo.hex}`);
+      if(prompt){
+      prompt.style.opacity = "1";
+      setTimeout(()=>prompt.style.opacity = "0",1000)
+      }
+
+     })
+      .catch(err => console.log("Error while copying hex value: " + err))     
+    }
+  
+  }
   return (
-      <div style={styles}>
+      <div style={styles} onClick={handelClick}>
         <div className="color-info" style={fontSize}>
           <div className="color-code">{colorInfo.hex}</div>
           <div className="color-name">{colorInfo.name}</div>
         </div>
+          <div id={`value-copied-prompt-${colorInfo.hex}`}className="value-copied-prompt" >Hex Copied</div>
       </div>
   );
 }
