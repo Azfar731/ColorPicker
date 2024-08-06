cf45a337a6a4fa67d38054a75cc3f62ed1c0de53
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-06 15:43:16 +0500

    Returning colorsInfo array from loader instead of the whole data.colors array

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,57 +1,54 @@
 import Pallet from "./Pallet";
 import InputForm from "./InputForm";
-import {getSearchParams} from "../utilityFunctions"
-import {  defer, Await ,useLoaderData } from "react-router-dom";
+import { getSearchParams } from "../utilityFunctions";
+import { defer, Await, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
 import "./HomePage.css";
 
+type Color = {
+  hex: string;
+  name: string;
+}
 
+function getColorsInfo(colorsArray) {
+  const colorInfo: Color[] = colorsArray.map(
+    (color) => ({ hex: color.hex.value, name: color.name.value })
+  );
+  return colorInfo;
+}
 
-async function fetchDataFromColorAPI(targetUrl: string){
+async function fetchDataFromColorAPI(targetUrl: string) {
   const response = await fetch(targetUrl);
   if (response.status !== 200) {
     throw new Error("Error occurred with API");
   }
   const data = await response.json();
- 
-  console.log('data fetched')
-  return data.colors
+  return getColorsInfo(data.colors);
 }
+
 export function loader({ request }) {
   const params = new URL(request.url).searchParams;
-  const {color,mode,count} = getSearchParams(params)
-  
+  const { color, mode, count } = getSearchParams(params);
+
   const baseUrl = "https://www.thecolorapi.com/scheme";
   const searchQuery = `?hex=${color}&mode=${mode}&count=${count}&format=json`;
-  
-  console.log("Loader function running")
- 
-  return defer({colorsArray: fetchDataFromColorAPI(baseUrl + searchQuery)});
+
+  return defer({ colorsInfo: fetchDataFromColorAPI(baseUrl + searchQuery) });
 }
 
-
 export default function HomePage() {
-  const loaderData = useLoaderData();
-  console.log(loaderData.colorsArray)
-  console.log("loaderData variable accessed")
+  const loaderData: {colorsInfo: Color[] } = useLoaderData();
   
 
-  function renderPallet(colorsArray){
-    const colorInfo = colorsArray.map((color) => ({hex: color.hex.value, name: color.name.value}));
-    console.log("In render function")
-    console.log(colorInfo)
-    return(
-      <Pallet colorsInfo={colorInfo} />
-    )
+  function renderPallet(colorsInfo: Color[]) {
+    return <Pallet colorsInfo={colorsInfo} />;
   }
 
   return (
     <main>
       <InputForm />
       <Suspense fallback={<h1>Loading....</h1>}>
-        <Await resolve={loaderData.colorsArray}>
-          {renderPallet}
-        </Await>
+        <Await resolve={loaderData.colorsInfo}>{renderPallet}</Await>
       </Suspense>
     </main>
   );
